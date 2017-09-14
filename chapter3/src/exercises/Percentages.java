aad888d91f659efee52f1a889f21fd70a690c508
mc307009 <michaelcurtis2421@gmail.com>
2017-09-14 09:28:43 -0500

    update 2 of 9/14

--- chapter3/src/exercises/Percentages.java
+++ chapter3/src/exercises/Percentages.java
@@ -2,8 +2,11 @@ package exercises;
 
 public class Percentages {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		double num1 = 2.50;
+		double num2 = 4.50;
+		
 
 	}
 
