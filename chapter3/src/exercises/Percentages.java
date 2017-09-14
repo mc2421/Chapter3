82aeb2ed8772940bccf7f437c4e316aa86e4ddc4
mc307009 <michaelcurtis2421@gmail.com>
2017-09-14 09:29:02 -0500

    9/14 3

--- chapter3/src/exercises/Percentages.java
+++ chapter3/src/exercises/Percentages.java
@@ -1,13 +1,27 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class Percentages {
 
 	public static void main(String[] args) 
 	{
 		double num1 = 2.50;
 		double num2 = 4.50;
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Enter num1 >>");
+		num1 = keyboard.nextInt();
+		System.out.print("Enter num2 >>");
+		num2 = keyboard.nextInt();
+	}
+	public static void computePercent(double x, double y)
+	{ 
+		double firstNumber;
+		double secondNumber;
 		
-
+		firstNumber
+		secondNumber
+		
 	}
 
 }
